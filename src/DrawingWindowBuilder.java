67e89c4d851ca65d6bedd5152efef52e8b4ec575
Zazu223 <zspilka@macalester.edu>
2019-04-18 22:53:09 -0500

    Working on Canvas Windwo stuff

--- src/DrawingWindowBuilder.java
+++ src/DrawingWindowBuilder.java
@@ -1,3 +1,4 @@
+import comp124graphics.CanvasWindow;
 import comp124graphics.GraphicsObject;
 import comp124graphics.GraphicsObserver;
 import javafx.scene.canvas.Canvas;
@@ -10,111 +11,36 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.concurrent.ConcurrentLinkedDeque;
 
+/**
+ * For now, abadoning this because its a little too convaluted/Complicated, will work on this more once I figure out how to add a png file to the canvas.
+ */
 
-public class DrawingWindowBuilder extends JPanel implements GraphicsObserver {
+public class DrawingWindowBuilder extends CanvasWindow {
 
     protected Canvas win;
-    protected Eyes aye = new Eyes();
-    protected JFrame winFrame;
 
-    private ConcurrentLinkedDeque<Image> gObjects;
 
 
     public void createWindow(String title, int winWidth, int winHeight) {
+        ImageIcon icon = new ImageIcon("blueEye.png");
+        java.awt.Image image = icon.getImage();
+
 //        GraphicsContext gc = win.getGraphicsContext2D();
         win = new Canvas();
         win.setHeight(winHeight);
         win.setWidth(winWidth);
-        setPreferredSize(new Dimension(winWidth, winHeight));
-        setBackground (Color.white);
-        winFrame = new JFrame (title);
-        winFrame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-        winFrame.getContentPane().add(this);
-        winFrame.pack();
-        winFrame.setVisible(true);
-        
 
 
-
-//        win.add(aye);
-    }
-
-    /**
-     * Adds the graphical object to the list of objects drawn on the canvas
-     * @param image
-     */
-    public void add(Image image){
-//        image.addObserver(this);
-        gObjects.add(image);
-        repaint();
-    }
-
-    /**
-     * Adds the graphical object to the list of objects drawn on the canvas
-     * at the position x, y.
-     *
-     * @param image  the graphical object to be drawn
-     * @param x        the x position of graphical object
-     * @param y        the y position of graphical object
-     */
-    public void add(Image image, double x, double y){
-//        image.ad(x, y);
-        this.add(image);
-//        gObject.addObserver(this);
-//        gObjects.add(gObject);
-//        repaint();
-    }
-
-    /**
-     * Removes the object from being drawn
-     * @param gObject
-     * @throws NoSuchElementException if gObject has not been added to the canvas
-     */
-    public void removeZ(GraphicsObject gObject){
-        gObject.removeObserver(this);
-        boolean success = gObjects.remove(gObject);
-        if (!success){
-            throw new NoSuchElementException("The object you want to remove has not been added to the canvaswindow. Perhaps it was already removed or was added to a GraphicsGroup instead of the canvas.");
-        }
-        repaint();
-    }
-
-    /**
-     * Removes all of the objects currently drawn on the canvas
-     */
-    public void removeAllZ(){
-        Iterator<Image> it = gObjects.iterator();
-        while(it.hasNext()){
-            Image obj = it.next();
-//            obj.removeObserver(this);
-            it.remove();
-        }
-        repaint();
     }
 
 
-    public DrawingWindowBuilder(String title, int width, int height){
-        createWindow(title, width,height);
+    public DrawingWindowBuilder(String title, int width, int height) {
+        super(title, width, height);
+        createWindow(title, width, height);
 //        System.out.println(win.getHeight());
 //        System.out.println(win.getWidth());
 
     }
 
 
-    public JFrame getWindowFrame(){
-        return winFrame;
-    }
-
-    /**
-     * Closes the canvas window.
-     */
-    public void closeWindow() {
-        winFrame.dispatchEvent(new WindowEvent(winFrame, WindowEvent.WINDOW_CLOSED));
-    }
-
-
-    @Override
-    public void graphicChanged(GraphicsObject changedObject) {
-
-    }
 }
--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,10 +1,23 @@
+import comp124graphics.GraphicsObject;
+import comp124graphics.GraphicsObserver;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class MainWindow {
 
-    public static DrawingWindowBuilder winwin;
+//    public static DrawingWindowBuilder winwin;
+    public static CanvasWindow win;
+//    protected Feature feature;
+
 
 
 
     public static void main(String[] args) {
-        winwin = new DrawingWindowBuilder("Main Window", 200, 500);
+        ImageIcon icon = new ImageIcon("blueEye.png");
+        java.awt.Image image = icon.getImage();
+
+        win = new CanvasWindow("Main Window", 200, 500);
+        win.add(image);
     }
 }
