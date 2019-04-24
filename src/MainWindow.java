05d6a079cc99b4e8cf0cfe5a60ef6502e3f14683
CrookedlyBent <35541263+CrookedlyBent@users.noreply.github.com>
2019-04-23 20:48:09 -0500

    Took away practice image

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -7,10 +7,9 @@ public class MainWindow{
     protected static Image image;
 
     public static void main(String[] args) {
-
-        image = new Image (0,0, "C:\\Users\\Linnea Prehn\\IdeaProjects\\Comp221Project\\Images\\blueEye.png");
+        
         win = new CanvasWindow("Avatar", 500, 500);
-        win.add(image);
+
 
         Feature [] spriteList = {new Eyes(), new Lips(), new Hair(), new Nose(), new Brow()};
         for (int i =0; i< spriteList.length; i++){
