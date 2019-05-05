8a7b00784d8c1085552223a5fb2994f3fc4acc5a
CrookedlyBent <35541263+CrookedlyBent@users.noreply.github.com>
2019-05-04 20:48:15 -0500

    Hair

--- src/Head.java
+++ src/Head.java
@@ -19,7 +19,7 @@ public class Head extends Feature {
 //        Image image5 = new Image (0,0, "Heads/Head5-1.png");
 //        spriteList.add(image5);
 
-        ImageIcon icon = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Heads/Head1-1.png");
+        ImageIcon icon = new ImageIcon("Heads/Head1.png");
 //        Image image = icon.getImage();
 //        image.getScaledInstance(2,5,50);
 
@@ -27,13 +27,13 @@ public class Head extends Feature {
         //        java.awt.Image image = new Image(0,0, "/Users/zazu/IdeaProjects/Comp221Project/Eyes/EyePair2_blue.png");
 //        java.awt.Image newImage = image;
         spriteList.add(icon);
-        ImageIcon icon2 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Heads/Head2-1.png");
+        ImageIcon icon2 = new ImageIcon("Heads/Head2.png");
         spriteList.add(icon2);
-        ImageIcon icon3 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Heads/Head3-1.png");
+        ImageIcon icon3 = new ImageIcon("Heads/Head3.png");
         spriteList.add(icon3);
-        ImageIcon icon4 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Heads/Head4-1.png");
+        ImageIcon icon4 = new ImageIcon("Heads/Head4.png");
         spriteList.add(icon4);
-        ImageIcon icon5 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Heads/Head5-1.png");
+        ImageIcon icon5 = new ImageIcon("Heads/Head5.png");
         spriteList.add(icon5);
     }
 }
