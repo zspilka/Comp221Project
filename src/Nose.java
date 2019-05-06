f8c13d071cb5dd644f851890f84d6d6ad46ec6a6
CrookedlyBent <35541263+CrookedlyBent@users.noreply.github.com>
2019-05-06 17:48:27 -0500

    Rearranged the order of nose and lips!

--- src/Nose.java
+++ src/Nose.java
@@ -24,42 +24,44 @@ public class Nose extends Feature {
     @Override
     protected void createSpriteList() {
         //Path
-        ImageIcon icon = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose1-1.png");
-        spriteList.add(icon);
 
-        ImageIcon icon2 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose2-1.png");
-        spriteList.add(icon2);
-
-        ImageIcon icon3 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose3-1.png");
-        spriteList.add(icon3);
-
-        ImageIcon icon4 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose4-1.png");
-        spriteList.add(icon4);
-
-        ImageIcon icon5 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose5-1.png");
-        spriteList.add(icon5);
-
-        ImageIcon icon6 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose6-1.png");
-        spriteList.add(icon6);
-
-//        //Relative Path
-//        ImageIcon icon = new ImageIcon("Nose/Nose1-1.png");
+//        ImageIcon icon = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose2-1.png");
 //        spriteList.add(icon);
 //
-//        ImageIcon icon2 = new ImageIcon("Nose/Nose2-1.png");
+//        ImageIcon icon2 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose3-1.png");
 //        spriteList.add(icon2);
 //
-//        ImageIcon icon3 = new ImageIcon("Nose/Nose3-1.png");
+//        ImageIcon icon3 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose4-1.png");
 //        spriteList.add(icon3);
 //
-//        ImageIcon icon4 = new ImageIcon("Nose/Nose4-1.png");
+//        ImageIcon icon4 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose1-1.png");
 //        spriteList.add(icon4);
 //
-//        ImageIcon icon5 = new ImageIcon("Nose/Nose5-1.png");
+//        ImageIcon icon5 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose5-1.png");
 //        spriteList.add(icon5);
 //
-//        ImageIcon icon6 = new ImageIcon("Nose/Nose6-1.png");
+//        ImageIcon icon6 = new ImageIcon("/Users/zazu/IdeaProjects/Comp221Project/Nose/Nose6-1.png");
 //        spriteList.add(icon6);
+
+//        //Relative Path
+
+        ImageIcon icon = new ImageIcon("Nose/Nose2-1.png");
+        spriteList.add(icon);
+
+        ImageIcon icon2 = new ImageIcon("Nose/Nose3-1.png");
+        spriteList.add(icon2);
+
+        ImageIcon icon3 = new ImageIcon("Nose/Nose4-1.png");
+        spriteList.add(icon3);
+
+        ImageIcon icon4 = new ImageIcon("Nose/Nose1-1.png");
+        spriteList.add(icon4);
+
+        ImageIcon icon5 = new ImageIcon("Nose/Nose5-1.png");
+        spriteList.add(icon5);
+
+        ImageIcon icon6 = new ImageIcon("Nose/Nose6-1.png");
+        spriteList.add(icon6);
 //
 
     }
